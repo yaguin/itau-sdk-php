9620d6466fda2c36f958a81473ef454d95b1e50d
Yago <yagof.3111@gmail.com>
2022-02-22 14:54:11 -0300

    add legal person

--- src/Models/PersonType.php
+++ src/Models/PersonType.php
@@ -11,4 +11,9 @@ class PersonType
      *@var string diretorio onde se encontra os certificados
      */
     public $numero_cadastro_pessoa_fisica;
+
+    /**
+     *@var string diretorio onde se encontra os certificados
+     */
+    public $numero_cadastro_nacional_pessoa_juridica;
 }
